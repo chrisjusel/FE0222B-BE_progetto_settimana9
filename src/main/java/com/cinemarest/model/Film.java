4df110af7d885ea37ba1d68af5f2cc2e7ea71d9f
chrisjusel <christian00.vitale@gmail.com>
2022-08-05 10:25:15 +0200

    Creazione models

---
+++ src/main/java/com/cinemarest/model/Film.java
@@ -0,0 +1,83 @@
+package com.cinemarest.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Film {
+
+	private Long id;
+
+	private String titolo;
+
+	private int anno;
+
+	private Regista regista;
+
+	private String tipo;
+
+	private int incasso;
+
+	public Film() {
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	public Long getId() {
+		return id;
+	}
+
+	@Column(nullable = false)
+	public String getTitolo() {
+		return titolo;
+	}
+
+	public int getAnno() {
+		return anno;
+	}
+
+	@ManyToOne
+	@JoinColumn(name = "film")
+	public Regista getRegista() {
+		return regista;
+	}
+
+	@Column(nullable = false)
+	public String getTipo() {
+		return tipo;
+	}
+
+	public int getIncasso() {
+		return incasso;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setTitolo(String titolo) {
+		this.titolo = titolo;
+	}
+
+	public void setAnno(int anno) {
+		this.anno = anno;
+	}
+
+	public void setRegista(Regista regista) {
+		this.regista = regista;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public void setIncasso(int incasso) {
+		this.incasso = incasso;
+	}
+
+}
---
+++ src/main/java/com/cinemarest/model/Regista.java
@@ -0,0 +1,76 @@
+package com.cinemarest.model;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Regista {
+
+	private Long id;
+
+	private String nome;
+
+	private String cognome;
+
+	private Date annoNascita;
+
+	private List<Film> film;
+
+	public Regista() {
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	public Long getId() {
+		return id;
+	}
+
+	@Column(nullable = false)
+	public String getNome() {
+		return nome;
+	}
+
+	@Column(nullable = false)
+	public String getCognome() {
+		return cognome;
+	}
+
+	@Column(nullable = false)
+	public Date getAnnoNascita() {
+		return annoNascita;
+	}
+
+	@OneToMany(mappedBy = "regista", cascade = CascadeType.ALL)
+	public List<Film> getFilm() {
+		return film;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public void setAnnoNascita(Date annoNascita) {
+		this.annoNascita = annoNascita;
+	}
+
+	public void setFilm(List<Film> film) {
+		this.film = film;
+	}
+
+}
